e8fde9d21c3b4b00ef2da3e0c180de003521fbf7
Bence <farkasbence2@gmail.com>
2025-05-26 20:00:43 +0200

    dev-en 8 as változás

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ console.log('4. dev ágon ismét egy változtatás!');
 console.log('5. dev ágon egy változtatás történt!');
 console.log('6. dev ágon egy változtatás!');
 console.log('7. dev ágon egy változtatás!');
+console.log('8. dev ágon egy változtatás!');
+8
