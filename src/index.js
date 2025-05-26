619a172b5836f40a65a50a5738e755555e6022de
Bence <farkasbence2@gmail.com>
2025-05-26 19:43:42 +0200

    5. dev változtatás

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ console.log('1. nodemon script-et felvettük!');
 console.log('2. dev ágra brancheltünk, ez az első változtatás!');
 console.log('3. dev ágon a második változtatás!');
 console.log('4. dev ágon ismét egy változtatás!');
+console.log('5. dev ágon egy változtatás történt!');
 
