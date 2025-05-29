a904954d9bb28f515e2186d3df30058339e305aa
Bence <farkasbence2@gmail.com>
2025-05-29 18:57:12 +0200

    dev ágban 14 sor létrejött

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ console.log('10. dev ágon egy változtatás! na ezzel vajon mi lesz?');
 console.log('11. dev ágon egy változtatás! na ezzel vajon mi lesz?');
 console.log('12. MASTER ÁG létrejött!');
 console.log('13. dev ág!');
+console.log('14. Elkezdtem valami munkát és be is fejeztem már');
+
 
 
 
